3c4ffbc88723dd6b06374a288d13efa5de81922e
han1202012 <octopus_truth@163.com>
2020-06-29 21:47:05 +0800

    update

--- app/src/main/java/kim/hsl/bm/MainActivity.java
+++ app/src/main/java/kim/hsl/bm/MainActivity.java
@@ -22,12 +22,49 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
+        Log.i("Bitmap", "getResources().getDisplayMetrics().densityDpi : " +
+                getResources().getDisplayMetrics().densityDpi +
+                " , getResources().getDisplayMetrics().density : " +
+                getResources().getDisplayMetrics().density);
+
         // 从资源文件中加载内存
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blog);
         // 打印 Bitmap 对象的宽高, 字节大小
-        Log.i("Bitmap", bitmap.getWidth() + " , " +
+        Log.i("Bitmap", "blog : " + bitmap.getWidth() + " , " +
                         bitmap.getHeight() + " , " +
                         bitmap.getByteCount());
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.blog_h);
+        // 打印 Bitmap 对象的宽高, 字节大小
+        Log.i("Bitmap", "blog_h : " + bitmap.getWidth() + " , " +
+                bitmap.getHeight() + " , " +
+                bitmap.getByteCount());
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.blog_m);
+        // 打印 Bitmap 对象的宽高, 字节大小
+        Log.i("Bitmap", "blog_m : " + bitmap.getWidth() + " , " +
+                bitmap.getHeight() + " , " +
+                bitmap.getByteCount());
+
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.blog_x);
+        // 打印 Bitmap 对象的宽高, 字节大小
+        Log.i("Bitmap", "blog_x : " + bitmap.getWidth() + " , " +
+                bitmap.getHeight() + " , " +
+                bitmap.getByteCount());
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.blog_xx);
+        // 打印 Bitmap 对象的宽高, 字节大小
+        Log.i("Bitmap", "blog_xx : " + bitmap.getWidth() + " , " +
+                bitmap.getHeight() + " , " +
+                bitmap.getByteCount());
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.blog_xxx);
+        // 打印 Bitmap 对象的宽高, 字节大小
+        Log.i("Bitmap", "blog_xxx : " + bitmap.getWidth() + " , " +
+                bitmap.getHeight() + " , " +
+                bitmap.getByteCount());
+
     }
 
     public native String stringFromJNI();
