2bf6e622107d33bab83cd9b2d972f8e6752562f5
octopus <octopus_truth@163.com>
2020-07-01 19:31:15 +0800

    update

--- app/src/main/java/kim/hsl/bm/utils/BitmapMemoryCache.java
+++ app/src/main/java/kim/hsl/bm/utils/BitmapMemoryCache.java
@@ -5,4 +5,16 @@ package kim.hsl.bm.utils;
  */
 public class BitmapMemoryCache {
     private static final String TAG = "BitmapMemoryCache";
+
+    /**
+     * 单例实现
+     */
+    private static BitmapMemoryCache INSTANCE;
+    private BitmapMemoryCache(){}
+    public static BitmapMemoryCache getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new BitmapMemoryCache();
+        }
+        return INSTANCE;
+    }
 }
